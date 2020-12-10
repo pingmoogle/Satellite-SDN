6e2c4a4369a64c267125025a7b51f6e2a1488c0d
Soowin <33149289+Soowin@users.noreply.github.com>
2020-12-10 20:34:57 +0800

    initiation Codes

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template(
+        "index.html",
+        title="Home Page"
+    )
+
+
+if __name__ == '__main__':
+    app.run()
