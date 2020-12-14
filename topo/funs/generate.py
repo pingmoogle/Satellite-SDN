6458877478c9e208da9b33e1edd548d14da7fa5d
zdkk <1040893382@qq.com>
2020-12-14 17:23:29 +0800

    替换所有节点图标

--- topo/funs/generate.py
+++ topo/funs/generate.py
@@ -2,20 +2,21 @@ from jinja2 import Environment, FileSystemLoader
 from pyecharts import options as opts
 from pyecharts.charts import Graph
 from pyecharts.globals import CurrentConfig
-from .dealJSON import  dealjson
+from topo.funs import dealJSON
 
 def newgraph():
     # TODO: json文件如何解析？
 
     str = "topo/data/topo.json"
-    nodes, links = dealjson(str)
+    nodes, links = dealJSON.dealjson(str)
     CurrentConfig.GLOBAL_ENV = Environment(loader=FileSystemLoader("templates"))
     c = (
         Graph(init_opts={
             "chart_id": "graph1",
             "width": "1108px"
         })
-        .add("", nodes, links, repulsion=3000)
+        .add("", nodes, links, repulsion=3000, gravity=0.6, symbol_size=50
+             , symbol="image://static/svgs/orange.svg")
         .set_global_opts(title_opts=opts.TitleOpts(title="Satellite-sdn topo"))
         .render("templates/topos/graph_page.html",
                 template_name="render/simple_chart.html")
